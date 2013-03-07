39705b7fc5b66555d74534704b876fd7d2fe0bce
luics <luics.xu@gmail.com>
2013-03-07 23:10:29 +0800

    lightbox

--- demo/web-fe/js/index.js
+++ demo/web-fe/js/index.js
@@ -6,7 +6,9 @@ Reveal.initialize({
     history: true,
     center: true,
 
-    theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+    //theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+    //transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+    theme: 'night', // available themes are in /css/theme
     transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
 
     // Optional libraries used to extend on reveal.js
