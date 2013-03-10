62bbec8370a2f4612ec402654cdac1bf55243517
luics <luics.xu@gmail.com>
2013-03-10 17:18:35 +0800

    close log

--- js/index.js
+++ js/index.js
@@ -8,12 +8,12 @@ $(function() {
     var $body = $(document.body);
     var $toolkit = $('#toolkit');
     $window.scroll(function(e) {
-        console.log('onscroll', $window.scrollTop(), $body.height(), $window.height());
+        //console.log('onscroll', $window.scrollTop(), $body.height(), $window.height());
 
         if ($toolkit.css('display') === 'none') {
-            console.log('toolkit display:none');
+            //console.log('toolkit display:none');
             if ($window.scrollTop() > $body.height() - $window.height()) {
-                console.log('toolkit expand');
+                //console.log('toolkit expand');
                 $toolkit.fadeIn(3000);
             }
         }
