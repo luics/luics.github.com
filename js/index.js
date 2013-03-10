468d519b33687a337392a084f672e6245d226c2a
luics <luics.xu@gmail.com>
2013-03-10 17:15:52 +0800

    waterfall?

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/**
+ * @author luics(luics.xu@gmail.com)
+ * @date 2013-03-10
+ */
+
+$(function() {
+    var $window = $(window);
+    var $body = $(document.body);
+    var $toolkit = $('#toolkit');
+    $window.scroll(function(e) {
+        console.log('onscroll', $window.scrollTop(), $body.height(), $window.height());
+
+        if ($toolkit.css('display') === 'none') {
+            console.log('toolkit display:none');
+            if ($window.scrollTop() > $body.height() - $window.height()) {
+                console.log('toolkit expand');
+                $toolkit.fadeIn(3000);
+            }
+        }
+    });
+});
