31c085e4e3ac9f8a16932785a6c3ae13c62ba3c0
鬼道 <xukai.xk@taobao.com>
2013-12-28 21:38:28 +0800

    精简描述、删除注释

--- js/index.js
+++ js/index.js
@@ -4,18 +4,30 @@
  */
 
 $(function() {
-    var $window = $(window);
-    var $body = $(document.body);
-    var $toolkit = $('#toolkit');
-    $window.scroll(function(e) {
-        //console.log('onscroll', $window.scrollTop(), $body.height(), $window.height());
 
-        if ($toolkit.css('display') === 'none') {
-            //console.log('toolkit display:none');
-            if ($window.scrollTop() > $body.height() - $window.height()) {
-                //console.log('toolkit expand');
-                //$toolkit.fadeIn(3000);
-            }
-        }
-    });
+    var disqus_shortname = 'luics';
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+
+//    var $window = $(window);
+//    var $body = $(document.body);
+//    var $toolkit = $('#toolkit');
+//    $window.scroll(function(e) {
+//        //console.log('onscroll', $window.scrollTop(), $body.height(), $window.height());
+//
+//        if ($toolkit.css('display') === 'none') {
+//            //console.log('toolkit display:none');
+//            if ($window.scrollTop() > $body.height() - $window.height()) {
+//                //console.log('toolkit expand');
+//                //$toolkit.fadeIn(3000);
+//            }
+//        }
+//    });
 });
