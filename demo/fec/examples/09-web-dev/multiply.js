e1d125fe11bebd9c86f0012213aa4cc616c63b2e
luics <luics.xu@gmail.com>
2016-05-02 23:57:19 +0800

    move fec to new repo web-dev

--- demo/fec/examples/09-web-dev/multiply.js
+++
@@ -1,9 +0,0 @@
-var line;
-for (var i = 1; i <= 9; i++) {
-  line = [];
-  for (var j = 1; j <= 9; j++) {
-    line.push(i + 'x' + j + '=' + (i * j));
-    line.push('\t');
-  }
-  console.log(line.join(''));
-}
